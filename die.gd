927038593a8b4cb9014f0a0872ef2bb8649260cb
cptmoo <68177916+cptmoo@users.noreply.github.com>
2025-10-05 13:12:48 +1100

    About to change again...

--- die.gd
+++ die.gd
@@ -1,20 +1,25 @@
-extends Node2D
+extends Button
 class_name Die 
+
+
 
 var letter: String:
 	set(value):
 		letter = value
 		if is_node_ready() and $LetterDisplay:
 			$LetterDisplay.text = value
+	get():
+		return letter
 
 
 
-func _init(letter: String):
+func _init(letter: String = ""):
 	self.letter = letter
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$LetterDisplay.text = letter
+	#$LetterDisplay.text = letter
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- moggle_board.gd
+++ moggle_board.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
-var size : Vector2i = Vector2i(4,4)
+var SIZE : int = 4
 
 var dice_letters: Array = ['AAEEGN', 'ABBJOO', 'ACHOPS', 'AFFKPS', 'AOOTTW', 'CIMOTU', 'DEILRX', 'DELRVY', 'DISTTY', 'EEGHNW', 'EEINSU', 'EHRTVW', 'EIOSST', 'ELRTTY', 'HIMNU1', 'HLNNRZ']
-@onready var ts : TileMapLayer = $DiceBoard
+
+@onready var grid = $GridContainer
+#@onready var ts : TileMapLayer = $DiceBoard
 #var dice: Arra
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(dice_letters[3])
-	print(ts.get_used_cells())
-
+	#print(ts.get_used_cells())
+	
+	for i in range(SIZE * SIZE):
+		var new_die = Die.new(dice_letters[i][0])
+		grid.add_child(new_die)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
